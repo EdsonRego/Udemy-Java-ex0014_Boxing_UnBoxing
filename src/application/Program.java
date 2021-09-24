f524c7be187effd1270eed221ea5df1cc7d78281
EdsonRego <edsonxrego@gmail.com>
2021-09-24 09:17:32 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = 20;
+		
+		Object obj = x;
+		
+		System.out.println(obj);
+		
+		int y = (int) obj;
+		
+		System.out.println(obj);
+		
+		sc.close();
+	}
+
+}
